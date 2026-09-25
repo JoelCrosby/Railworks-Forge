16f9cf616fa123255b9a702b1f0cd863b157b2bc
Joel Crosby <joelcrosby@live.co.uk>
2026-09-26 00:30:50 +0100

    update styling to make it more modern

--- src/RailworksForge/Views/TemplatedControls/TableHeader.axaml.cs
+++ src/RailworksForge/Views/TemplatedControls/TableHeader.axaml.cs
@@ -1,5 +1,6 @@
 using Avalonia;
 using Avalonia.Controls.Primitives;
+using Avalonia.Metadata;
 
 namespace RailworksForge.Views.TemplatedControls;
 
@@ -8,9 +9,19 @@ public class TableHeader : TemplatedControl
     public static readonly StyledProperty<string> TextProperty =
         AvaloniaProperty.Register<TableHeader, string>(nameof(Text));
 
+    public static readonly StyledProperty<object?> ToolsProperty =
+        AvaloniaProperty.Register<TableHeader, object?>(nameof(Tools));
+
     public string Text
     {
         get => GetValue(TextProperty);
         set => SetValue(TextProperty, value);
+    }
+
+    [Content]
+    public object? Tools
+    {
+        get => GetValue(ToolsProperty);
+        set => SetValue(ToolsProperty, value);
     }
 }
